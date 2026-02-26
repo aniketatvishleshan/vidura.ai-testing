9d2b88b73ffc3252a27514953b53aa7aa2a77fe5
aniketvss <aniket.kadam@vishleshan.net>
2026-02-26 16:00:12 +0530

    fun and class added

--- test.py
+++ test.py
@@ -5,3 +5,10 @@ print("new extractor file added")
 print("only takes latest commit")
 
 print("semantic AST added")
+
+def add(a, b):
+    return a + b
+
+class Engine:
+    def run(self):
+        pass
